8cc7836f18c0dfb5de51d23b1cca767df95b9be2
dragon chen <clplain@hotmail.com>
2017-03-31 17:43:48 +0800

    Ui change

--- poc/src/app.js
+++ poc/src/app.js
@@ -4,16 +4,22 @@ import { IndexRoute, Router, Route } from 'react-router';
 import createHistory from 'history/lib/createHashHistory';
 const history = createHistory( { queryKey: false } );
 import App from './js/components/App';
-import Home from './js/components/Home';
-import fileUpload from './js/components/fileUpload';
-import dashboard from './js/dashboard/demo';
+import Logout from './js/components/Logout';
+import RiskSourceConfig from './js/riskSourceConfig/RiskSourceConfig';
+import Dashboard from './js/dashboard/dashboard';
+import Adjustment from './js/adjustment/adjustment';
+import DataExtraction from './js/dataextraction/dataextraction';
 import PageNotFound from './js/components/PageNotFound';
+
 const routes = (
   <Router history={ history }>
     <Route path='/' component={ App }>
-      <IndexRoute component={ Home } />
-      <Route path='fileUpload' component={ fileUpload }/>
-      <Route path='dashboard' component={ dashboard } />
+      <IndexRoute component={ Dashboard } />
+      <Route path='RisksourceConfig' component={ RiskSourceConfig }/>
+      <Route path='Adjustment' component={ Adjustment }/>
+      <Route path='DataExtraction' component={ DataExtraction } />
+      <Route path='Dashboard' component={ Dashboard } />
+      <Route path='Logout' component={ Logout } />
       <Route path='*' component={ PageNotFound }/>
     </Route>
   </Router>
